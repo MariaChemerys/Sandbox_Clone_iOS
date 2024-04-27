4830fb712986509d617fef5e950ec359bd75eed4
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-04-27 10:48:17 +0200

    Added viewmodels files

---
+++ Sandbox Clone iOS/ViewMoldels/NumberedPictureColoredViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  NumberedPictureViewModel.swift
+//  Sandbox Clone
+//
+//  Created by Mariia Chemerys on 17.11.2023.
+//
+
+import Foundation
+
+class NumberedPictureColoredViewModel{
+    var numberedPicturesColored=[
+    NumberedPictureColored(id: 0, imageNameColored: "penguin colored"),
+    NumberedPictureColored(id: 1, imageNameColored: "boat colored"),
+    NumberedPictureColored(id: 2, imageNameColored: "solar system colored"),
+    NumberedPictureColored(id: 3, imageNameColored: "raspberry colored"),
+    NumberedPictureColored(id: 4, imageNameColored: "rocket colored"),
+    NumberedPictureColored(id: 5, imageNameColored: "panda colored"),
+    NumberedPictureColored(id: 6, imageNameColored: "tyrannosaurus colored"),
+    NumberedPictureColored(id: 7, imageNameColored: "ufo colored"),
+    NumberedPictureColored(id: 8, imageNameColored: "volcano colored"),
+    NumberedPictureColored(id: 9, imageNameColored: "hedgehog colored"),
+    NumberedPictureColored(id: 10, imageNameColored: "parrot colored"),
+    NumberedPictureColored(id: 11, imageNameColored: "owl colored"),
+    NumberedPictureColored(id: 12, imageNameColored: "beaver colored")
+    ]
+}
---
+++ Sandbox Clone iOS/ViewMoldels/NumberedPicturebwViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  NumberedPicturebwViewModel.swift
+//  Sandbox Clone
+//
+//  Created by Mariia Chemerys on 17.11.2023.
+//
+
+import Foundation
+
+class NumberedPicturebwViewModel{
+    var numberedPicturesbw=[
+    NumberedPicturebw(id: 0, name: "penguin", imageNamebw: "penguin black and white"),
+    NumberedPicturebw(id: 1, name: "boat", imageNamebw: "boat black and white"),
+    NumberedPicturebw(id: 2, name: "solar system", imageNamebw: "solar system black and white"),
+    NumberedPicturebw(id: 3, name: "raspberry", imageNamebw: "raspberry black and white"),
+    NumberedPicturebw(id: 4, name: "rocket", imageNamebw: "rocket black and white"),
+    NumberedPicturebw(id: 5, name: "panda", imageNamebw: "panda black and white"),
+    NumberedPicturebw(id: 6, name: "tyrannosaurus", imageNamebw: "tyrannosaurus black and white"),
+    NumberedPicturebw(id: 7, name: "ufo", imageNamebw: "ufo black and white"),
+    NumberedPicturebw(id: 8, name: "volcano", imageNamebw: "volcano black and white"),
+    NumberedPicturebw(id: 9, name: "hedgehog", imageNamebw: "hedgehog black and white"),
+    NumberedPicturebw(id: 10, name: "parrot", imageNamebw: "parrot black and white"),
+    NumberedPicturebw(id: 11, name: "owl", imageNamebw: "owl black and white"),
+    NumberedPicturebw(id: 12, name: "beaver", imageNamebw: "beaver black and white")
+    ]
+}
