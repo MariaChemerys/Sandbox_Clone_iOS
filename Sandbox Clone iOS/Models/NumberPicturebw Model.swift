8f8c0cfb6f0afd27ad58caf51de435d2a6d190f7
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-04-27 10:46:08 +0200

    Added models

---
+++ Sandbox Clone iOS/Models/NumberPicturebw Model.swift
@@ -0,0 +1,16 @@
+//
+//  NumberPicturebw.swift
+//  Sandbox Clone
+//
+//  Created by Mariia Chemerys on 17.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct NumberedPicturebw: Identifiable{
+    
+    var id: Int
+    var name: String
+    var imageNamebw: String
+}
---
+++ Sandbox Clone iOS/Models/NumberedPictureColored Model.swift
@@ -0,0 +1,15 @@
+//
+//  NumberedPicture.swift
+//  Sandbox Clone
+//
+//  Created by Mariia Chemerys on 17.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct NumberedPictureColored: Identifiable{
+    
+    var id: Int
+    var imageNameColored: String
+}
